5af61564e3d9b8d3dd149796256cd1ddeaaa85e6
Amir Khaki <amirkhaki995@gmail.com>
2022-12-11 14:36:26 +0330

    init cobra project

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 Amir Khaki <amirkhaki995@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "kit",
+	Short: "simple git implementation in golang",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 Amir Khaki <amirkhaki995@gmail.com
+
+*/
+package main
+
+import "github.com/amirkhaki/kit/cmd"
+
+func main() {
+	cmd.Execute()
+}
